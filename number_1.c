345e79e450b235d2f7a5a2e0f3717b9497f02e0b
Uttam <utam.shrestha65@gmail.com>
2022-11-24 21:19:53 +0545

    tested and works well

---
+++ number_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, sum;
+    printf("Enter two numbers to find the sum: ");
+    scanf("%d%d", &a, &b);
+    sum = a + b;
+    printf("The sum of two numbers is %d", sum);
+}
+
+// How to run your code in VS Code Terminal (Ignore it.)
+// gcc filename with extension
+// ./a.exe
