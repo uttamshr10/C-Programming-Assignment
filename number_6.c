c3aeccf9d0ffcf60ee125d6506f4ee71018ede55
Uttam <utam.shrestha65@gmail.com>
2022-11-25 20:31:23 +0545

    testing

--- number_6.c
+++ number_6.c
@@ -1,1 +1,14 @@
 // 6. WAP to display factorial of a given number N
+
+#include <stdio.h>
+int main()
+{
+    int i, n, factorial = 1;
+    printf("Enter a number to find the factorial: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        factorial *= i;
+    }
+    printf("The factorial of %d is %d.", n, factorial);
+}
