3c90d5fea4bb4ace4be7aae475c208e33c236e3c
Uttam <utam.shrestha65@gmail.com>
2022-11-25 22:38:54 +0545

    tested and works well

--- number_7.c
+++ number_7.c
@@ -1,1 +1,14 @@
 // 7. WAP to read first 20 number and display only sum of even numbers.
+
+#include <stdio.h>
+int main()
+{
+    int i, n, sum = 0;
+    printf("Enter a number to check the sum of upto that number: ");
+    scanf("%d", &n);
+    for (i = 2; i <= n; i += 2)
+    {
+        sum += i;
+    }
+    printf("Sum of all numbers from 1 to %d is %d.", n, sum);
+}
