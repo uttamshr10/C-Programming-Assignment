ff424a421c7f7f95aa290546c6ee91f0c41146c1
Uttam <utam.shrestha65@gmail.com>
2022-11-25 20:20:00 +0545

    tested and works well

--- number_5.c
+++ number_5.c
@@ -1,1 +1,14 @@
 // 5. WAP to find the sum of the series 1+2+3+4….. Up to entered n numbers.
+
+#include <stdio.h>
+int main()
+{
+    int n, i, sum = 0;
+    printf("Enter a number to find the sum of it's series: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    printf("The sum of the series of %d is %d.", n, sum);
+}
