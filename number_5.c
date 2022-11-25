b43c06d5ed414441d1637ca48adcd1807807a062
Uttam <utam.shrestha65@gmail.com>
2022-11-25 20:27:16 +0545

    table logic comment added

--- number_5.c
+++ number_5.c
@@ -12,3 +12,13 @@ int main()
     }
     printf("The sum of the series of %d is %d.", n, sum);
 }
+
+/*
+Ignore it.
+i   |   n = 5(suppose)    |   sum
+1   |   1 <= 5            |   0 + 1 = 1
+2   |   2 <= 5            |   1 + 2 = 3
+3   |   3 <= 5            |   3 + 3 = 6
+4   |   4 <= 5            |   6 + 4 = 10
+5   |   5 <= 5            |   10 + 5 = 15
+*/
