7429024396a392b80db93e21314acf5eccabdba1
Uttam <utam.shrestha65@gmail.com>
2022-11-25 22:25:26 +0545

    tested and works wellL

--- number_8.c
+++ number_8.c
@@ -1,1 +1,16 @@
 // 8. WAP to read a number n and display all of its divisors.
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    printf("Enter a number to find all of it's divisors: ");
+    scanf("%d", &n);
+    printf("\nAll of %d divisors are\n", n);
+    for (i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            printf("%d\n", i);
+        }
+    }
+}
